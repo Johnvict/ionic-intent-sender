04402bc28a4edef8294db8d2adcddfba22daddc5
Johnvict <Johnvict20@gmail.com>
2020-11-16 14:27:56 +0100

    Added Platform Android, installed Plugins and created form UI

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-folder',
-  templateUrl: './folder.page.html',
-  styleUrls: ['./folder.page.scss'],
+	selector: 'app-folder',
+	templateUrl: './folder.page.html',
+	styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
+	public folder: string;
+	intentSenderForm: FormGroup = new FormGroup({
+		phone: new FormControl('', [Validators.required, Validators.pattern('[0]{1}[7-9]{1}[0-1]{1}[0-9]{8}')]),
+		name: new FormControl('Johnvict', [Validators.required])
+	});
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+	constructor() { }
 
-  ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
-  }
+	ngOnInit() {
+		//
+	}
 
+	sendIntent() {
+		//
+	}
 }
